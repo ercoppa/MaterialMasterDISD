5d8d4313a564b514442cbf68ad0a07ff2ce2f5c7
Emilio Coppa <ercoppa@gmail.com>
2018-07-07 09:56:51 +0100

    fix 2 degree

--- hadoop/2-degree_calculator/solution/reducer.py
+++ hadoop/2-degree_calculator/solution/reducer.py
@@ -6,20 +6,16 @@ current = 0
 lastKey = None
 
 for line in sys.stdin:
-    line = line.strip()
-    key, value = line.split('\t')
-    try:
-        value = value
-        if value == lastKey: # skip self edges
-            continue
-        if key != lastKey:
-            if lastKey is not None:
-                print '%s\t%d' % (lastKey, current)
-            lastKey = key
-            current = 0
-        current += 1
-    except ValueError:
-        pass
+	line = line.strip()
+	key, value = line.split('\t')
+	if value == lastKey: # skip self edges
+		continue
+	if key != lastKey:
+		if lastKey is not None:
+			print '%s\t%d' % (lastKey, current)
+		lastKey = key
+		current = 0
+	current += 1
 
 if lastKey is not None:
-    print '%s\t%d' % (lastKey, current)
+	print '%s\t%d' % (lastKey, current)
